f3ef2e4b8ebe8682b9152f7f42d90cac9c384154
Diego Ciudad Real <diegociudadreale@gmail.com>
2023-11-08 15:17:07 -0500

    centered a div?

--- src/pages/my-components/navbar.js
+++ src/pages/my-components/navbar.js
@@ -3,20 +3,37 @@ import React from 'react';
 
 function Navbar() {
   return (
+    <>
     <nav width="100%">
+      <div>
+
       <a href="/" class="underline-hover">
         Home
       </a>
-      <a href="https://drive.google.com/file/d/13XTRj41Dq8-jOmIHfNdUvYYIV7KSzvNg/view?usp=sharing" class="underline-hover">
-        Resume
-      </a>
-      <a href="/projects" class="underline-hover">
+      </div>
+      <div class="navbarmid">
+
+      <a href="/projects" class="underline-hover centeredtxt">
         Projects
       </a>
+      </div>
+
+      <div>
+      <a href="https://drive.google.com/file/d/13XTRj41Dq8-jOmIHfNdUvYYIV7KSzvNg/view?usp=sharing" class="underline-hover">
+        Resume  
+      </a>
+      </div>
+    </nav>
+
+    <nav width="100%">
+      <div>
+
       <a href="/about" class="underline-hover">
         About Me
       </a>
+      </div>
     </nav>
+    </>
   );
 }
 
